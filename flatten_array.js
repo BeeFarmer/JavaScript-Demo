adf08231a01e94435c628db7138886be5fe32d96
BeeFarmer <minkunl@uci.edu>
2019-07-19 16:54:12 -0700

    Michael

---
+++ flatten_array.js
@@ -0,0 +1,59 @@
+"use strict";
+
+/* 
+ * flatten array using Depth-first Search
+ * assuming elements in arr are either number or array
+ */
+function dfsFlatten(arr) {
+  const res = [];
+  dfs(arr, res);
+
+  return res;
+}
+
+// helper function for dfsFlatten
+function dfs(item, res) {
+  if (typeof item === "number") {
+    res.push(item);
+    return ;
+  }
+
+  for (let i = 0; i < item.length; ++i) {
+    dfs(item[i], res);
+  }
+}
+
+
+/* 
+ * flatten array using Breath-first Search
+ * assuming elements in arr are either number or array
+ */
+function bfsFlatten(arr) {
+  const res = [];
+  const queue = arr;
+  let item;
+
+  while (queue.length) {
+    item = queue.shift();
+    if (typeof item === "number") {
+      res.push(item);
+    } else {
+      queue.push(...item);
+    }
+  }
+
+  return res;
+}
+
+
+const raw_input = [
+  1,
+  2,
+  [3,4, [5,6,0]],
+  4,
+  [3,7],
+  0
+];
+
+console.log("DFS:", dfsFlatten(raw_input));
+console.log("BFS:", bfsFlatten(raw_input));
